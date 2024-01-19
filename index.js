6fc4223aa67911c9c8b23345089311eeecb5cdfd
Ryan Chou <swchou524@gmail.com>
2024-01-19 11:38:15 +0800

    完成 products 並將檔名改成 index
    將原本 index 改為 login

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
-import {createApp, ref} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import {createApp, onMounted, ref} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
 const URL = 'https://ec-course-api.hexschool.io/v2'
 const PATH = 'ryann'
 
-const app = {
+createApp({
   setup() {
-    const email = ref('')
-    const password = ref('')
-    const isFailed = ref(false)
-
-    const login = async () => {
-      try {
-        isFailed.value = false
-        const res = await axios.post(`${URL}/admin/signin`, {
-          username: email.value,
-          password: password.value
-        })
-        console.log(res)
-  
-        const { token, expired } = res.data
-        document.cookie = `hexToken=${token}; expires=${new Date(expired)};`
-        location.href= './products.html'
-      } catch(err) {
-        console.log(err)
-        isFailed.value = true
-        password.value = ''
-      }
-      
-
+    const products = ref([])
+    const product = ref({})
+    
+    const showProduct = (id) => {
+      product.value = products.value.filter(product => product.id === id)[0]
     }
+
+    const checkLogin = async () => {
+      try {
+        const token = document.cookie.replace(
+          /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
+          "$1",
+        )
+        console.log(token)
+        axios.defaults.headers.common['Authorization'] = token
+
+        const res = await axios.post(`${URL}/api/user/check`)
+        console.log(res.data)
+      } catch(err) {
+        alert(err.response.data.message)
+        window.location = 'login.html'
+      }
+    }
+
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(`${URL}/api/${PATH}/admin/products`)
+        console.log(res)
+        products.value = res.data.products
+      } catch(err) {
+        alert(err.response.data.message)
+      }
+    }
+
+    onMounted(async () => {
+      try {
+        await checkLogin()
+        await getProducts()
+      } catch (err) {
+        console.error('出現未經處理的錯誤')
+      }
+    })
+
     return {
-      email,
-      password,
-      isFailed,
-      login
+      products,
+      product,
+      showProduct
     }
   }
-}
-
-createApp(app).mount('#app')
+}).mount('#app')
